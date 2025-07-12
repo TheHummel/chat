ba34210d92ed230d7df1cd5b22f1725aacfe13bd
TheHummel <jannisalsbach@gmail.com>
2025-07-12 22:11:43 +0200

    cmt out sidebar header and footer

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -12,35 +12,36 @@ import {
   SidebarRail,
 } from "@/components/ui/sidebar"
 import { ThreadList } from "./assistant-ui/thread-list"
+import { ModelSelector } from "./model-selector"
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar {...props}>
-      <SidebarHeader>
+      {/* <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-                <Link href="https://assistant-ui.com" target="_blank">
-                  <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                    <MessagesSquare className="size-4" />
-                  </div>
-                  <div className="flex flex-col gap-0.5 leading-none">
-                    <span className="font-semibold">assistant-ui</span>
-                  </div>
-                </Link>
-              </SidebarMenuButton>
+              <Link href="https://assistant-ui.com" target="_blank">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <MessagesSquare className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-semibold">assistant-ui</span>
+                </div>
+              </Link>
+            </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
-      </SidebarHeader>
+      </SidebarHeader> */}
       <SidebarContent>
         <ModelSelector />
         <ThreadList />
       </SidebarContent>
-      
+
       <SidebarRail />
-      <SidebarFooter>
+      {/* <SidebarFooter>
         <SidebarMenu>
-         
+
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <Link href="https://github.com/assistant-ui/assistant-ui" target="_blank">
@@ -53,10 +54,10 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                 </div>
               </Link>
             </SidebarMenuButton>
-            
+
           </SidebarMenuItem>
         </SidebarMenu>
-      </SidebarFooter>
+      </SidebarFooter> */}
     </Sidebar>
   )
 }
